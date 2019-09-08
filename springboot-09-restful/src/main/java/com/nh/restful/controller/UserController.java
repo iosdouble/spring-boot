933fa7ac472db7efad9199d54fe1b6e1b5fae24c
nihui <18202504057@163.com>
2019-09-08 19:48:33 +0800

    commit swagger-ui

--- springboot-09-restful/src/main/java/com/nh/restful/controller/UserController.java
+++ springboot-09-restful/src/main/java/com/nh/restful/controller/UserController.java
@@ -2,6 +2,7 @@ package com.nh.restful.controller;
 
 import com.nh.restful.bean.User;
 import com.nh.restful.service.UserService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.validation.annotation.Validated;
@@ -15,22 +16,44 @@ import java.util.List;
  * @Date 2019/9/8 11:53 AM
  * @Created by nihui
  */
+@Api(value = "用户",description = "用户操作API",position = 100,protocols = "http")
 @RestController
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @ApiOperation(
+            value = "获取用户列表",
+            notes = "获取所有用户信息",
+            produces = "application/json,application/xml",
+            consumes = "application/json,application/xml",
+            response = List.class
+    )
     @GetMapping("/allUser")
     public List<User> getAllUser(){
         return userService.getAllUser();
     }
 
+    @ApiOperation(value = "获取用户信息",notes = "获取单个用户的信息")
+    @ApiResponses({
+            @ApiResponse(code=100,message = "请求参数有错"),
+            @ApiResponse(code=101,message = "未授权"),
+            @ApiResponse(code=103,message = "禁止访问"),
+            @ApiResponse(code=104,message = "请求路径不存在"),
+            @ApiResponse(code=105,message = "服务器内部错误"),
+    })
     @GetMapping("/getOneUser")
     public User getUserById(@RequestParam Integer id){
         return userService.getUserById(id);
     }
 
+    @ApiOperation(value = "增加用户操作",notes = "用户创建用户接口")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id",value = "用户ID",required = true,dataType = "Integer",paramType = "query"),
+            @ApiImplicitParam(name = "name",value = "用户名",required = true,dataType = "String",paramType = "query"),
+            @ApiImplicitParam(name = "password",value = "密码",required = true,dataType = "String",paramType = "query")
+    })
     @PostMapping("/addUser")
     public int addUser(@RequestBody User user){
         return userService.addUser(user);
